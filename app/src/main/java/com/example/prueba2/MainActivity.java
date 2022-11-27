2ce94d75a15bb7826a7dbc8a3d24eb1aaea48039
Afro <alejandro.carrascons@gmail.com>
2022-11-26 21:56:25 -0300

    intento de envio de datos

--- app/src/main/java/com/example/prueba2/MainActivity.java
+++ app/src/main/java/com/example/prueba2/MainActivity.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private Button btnBuscar,btnIngresar,btnModificar;
     private TextInputLayout tilTitulo,tilDescripcion;
+    public ArrayList<Tarea> listaTarea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
         referencias();
         eventos();
+
+
     }
     private void segundaActividad(){
         Intent segundaActividad = new Intent(this,MainActivity2.class);
         startActivity(segundaActividad);
+
+
     }
     //region referencias y eventos
     private void referencias(){
@@ -32,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         btnModificar = findViewById(R.id.btnModificar);
         tilTitulo = findViewById(R.id.tilTitulo);
         tilDescripcion = findViewById(R.id.tilDescripcion);
+        listaTarea = new ArrayList<Tarea>();
     }
     private void eventos(){
         btnBuscar.setOnClickListener(new View.OnClickListener() {
@@ -39,8 +48,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 segundaActividad();
             }
-        });
-    }
+        }
+        )
+        ;
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enviarDatos();
+            }
+        });}
+
     //endregion
+        private void enviarDatos(){
+            Intent sendDatos = new Intent(this,MainActivity2.class);
+            String titulo = tilTitulo.getEditText().getText().toString();
+            String descripcion = tilDescripcion.getEditText().getText().toString();
+            sendDatos.putExtra("sendTitulo",titulo);
+            sendDatos.putExtra("sendDescripcion",descripcion);
+            startActivity(sendDatos);
+        }
+
 
 }
--- app/src/main/java/com/example/prueba2/MainActivity2.java
+++ app/src/main/java/com/example/prueba2/MainActivity2.java
@@ -3,21 +3,64 @@ package com.example.prueba2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity2 extends AppCompatActivity {
     private TextInputLayout tilFiltrar;
     private Button btnFiltrar;
+    private ArrayAdapter<Tarea> adaptadorTareas;
+    private ArrayList<Tarea> tareaLista;
+    private ListView lvTarea;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-    }
+
+        referencias();
+        obtenerDatos();
+        eventos();
+        adaptadorTareas = new ArrayAdapter<Tarea>(this,android.R.layout.simple_list_item_1,tareaLista);
+        lvTarea.setAdapter(adaptadorTareas);
+
+
+
+        }
 
     private void referencias(){
         tilFiltrar = findViewById(R.id.tilFiltrar);
         btnFiltrar = findViewById(R.id.btnFiltrar);
+        tareaLista = new ArrayList<Tarea>();
+        lvTarea = findViewById(R.id.lvTarea);
+
+    }
+    private void eventos(){
+        btnFiltrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Tarea listaTarea = (Tarea)getIntent().getSerializableExtra("sendTarea");
+            }
+        });
+    }
+    private void obtenerDatos(){
+
+        String recibirTitulo = getIntent().getExtras().getString("sendTitulo");
+        String recibirdescripcion = getIntent().getExtras().getString("sendDescripcion");
+        Tarea t = new Tarea();
+        t.setTitulo(recibirTitulo);
+        t.setDescripcion(recibirdescripcion);
+        tareaLista.add(t);
+
+
     }
 }
